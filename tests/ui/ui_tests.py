54dd1c34ee35751f5ecb483f38f313213aa94204
Kuba <janochajakub02@gmail.com>
2025-11-17 12:30:45 +0100

    styling

--- tests/ui/ui_tests.py
+++ tests/ui/ui_tests.py
@@ -18,7 +18,7 @@ def driver():
     driver.quit()
 
 
-def TC_UI_01_test_go_to_main_by_logo(driver):
+def test_TC_UI_01_test_go_to_main_by_logo(driver):
     driver.get("https://practicesoftwaretesting.com/product"
                "/01KA6SR9JG4P594K2BBFYFTG0E")
     wait = WebDriverWait(driver, 5)
@@ -32,7 +32,7 @@ def TC_UI_01_test_go_to_main_by_logo(driver):
     driver.quit()
 
 
-def TC_UI_05_test_go_to_pliers(driver):
+def test_TC_UI_05_test_go_to_pliers(driver):
     driver.get("https://practicesoftwaretesting.com")
     time.sleep(2)
     wait = WebDriverWait(driver, 5)
@@ -52,7 +52,7 @@ def TC_UI_05_test_go_to_pliers(driver):
     driver.quit()
 
 
-def TC_UI_02_test_login_success(driver):
+def test_TC_UI_02_test_login_success(driver):
     driver.get("https://practicesoftwaretesting.com/auth/login")
 
     wait = WebDriverWait(driver, 10)
@@ -81,7 +81,7 @@ def TC_UI_02_test_login_success(driver):
     assert sign_out_btn.is_displayed()
 
 
-def TC_UI_06_test_login_invalid_credentials(driver):
+def test_TC_UI_06_test_login_invalid_credentials(driver):
     driver.get("https://practicesoftwaretesting.com/auth/login")
 
     wait = WebDriverWait(driver, 10)
@@ -105,7 +105,7 @@ def TC_UI_06_test_login_invalid_credentials(driver):
     assert "Invalid email or password" in error.text
 
 
-def TC_UI_03_test_admin_reports_chart_visible(driver):
+def test_TC_UI_03_test_admin_reports_chart_visible(driver):
     driver.get("https://practicesoftwaretesting.com/auth/login")
     wait = WebDriverWait(driver, 10)
 
@@ -137,7 +137,7 @@ def TC_UI_03_test_admin_reports_chart_visible(driver):
                                   "dashboardzie!")
 
 
-def TC_UI_04_test_admin_reports_latest_orders_table(driver):
+def test_TC_UI_04_test_admin_reports_latest_orders_table(driver):
     driver.get("https://practicesoftwaretesting.com/auth/login")
     wait = WebDriverWait(driver, 10)
 
@@ -175,7 +175,7 @@ def TC_UI_04_test_admin_reports_latest_orders_table(driver):
         "$"), "Kolumna 'Total' nie zawiera prawidłowej kwoty!"
 
 
-def TC_UI_08_test_add_to_cart_positive(driver):
+def test_TC_UI_08_test_add_to_cart_positive(driver):
     driver.get("https://practicesoftwaretesting.com/product"
                "/01KA6X65BES7CSX4Z0MJ7WVZ9V")
     wait = WebDriverWait(driver, 5)
@@ -193,7 +193,7 @@ def TC_UI_08_test_add_to_cart_positive(driver):
                                     "checkout page")
 
 
-def TC_UI_07_test_add_to_cart_negative(driver):
+def test_TC_UI_07_test_add_to_cart_negative(driver):
     driver.get("https://practicesoftwaretesting.com/product"
                "/01KA6X65BJ12S27D9YBQ3ZSMA8")
     wait = WebDriverWait(driver, 5)
